4bbac873f162a26274d10f8570ee640c670252b3
imadr <hi@imadr.me>
2024-02-26 15:56:21 +0100

    more cmake stuff

---
+++ samples/02-cubes/cubes.cpp
@@ -0,0 +1,34 @@
+#include "error.h"
+#include "game.h"
+#include "logger.h"
+
+using namespace blaz;
+
+int main() {
+    Game game;
+
+    Error err = game.load_game("data/game.cfg");
+    if (err) {
+        logger.error(err);
+    }
+
+    err = game.m_window.init("02-cubes");
+    if (err) {
+        logger.error(err);
+        return 1;
+    }
+
+    err = game.m_renderer.init(&game);
+    if (err) {
+        logger.error(err);
+        return 1;
+    }
+
+    while (game.m_window.event_loop()) {
+        game.m_renderer.draw();
+    }
+
+    game.m_window.close();
+
+    return 0;
+}
---
+++ samples/02-cubes/data/default.vert.glsl
@@ -0,0 +1,10 @@
+#version 330 core
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec3 a_color;
+
+out vec3 v_color;
+
+void main() {
+    v_color = a_color;
+    gl_Position = vec4(a_position, 1.0);
+}
