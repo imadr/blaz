ff7d37789b0e22aa71b89a47ece8f364492341ce
imadr <hi@imadr.me>
2024-03-24 22:30:58 +0100

    restart adding d3d11

--- samples/03-pbr/main.cpp
+++ samples/03-pbr/main.cpp
@@ -21,19 +21,24 @@ int main() {
 
     game.m_renderer.add_mesh(make_uv_sphere(64, 64));
 
-    game.m_current_level->m_scene.add_node(
-        Node{
-            .m_name = "sphere",
-            .m_position = Vec3(0, 0, 0),
-            .m_rotation = Quat::from_euler(Vec3(0, 0, 0)),
-        },
-        "root_node");
-    game.m_renderer.add_renderable(Renderable{
-        .m_name = "sphere",
-        .m_tags = {"pbr"},
-        .m_mesh = 0,
-        .m_node = game.m_current_level->m_scene.m_nodes_ids["sphere"],
-    });
+    for (u32 i = 0; i < 3; i++) {
+        for (u32 j = 0; j < 3; j++) {
+            str id = "sphere_" + std::to_string(i) + "_" + std::to_string(j);
+            game.m_current_level->m_scene.add_node( 
+                Node{
+                    .m_name = id,
+                    .m_position = Vec3((-1.0f + f32(i)) * 2.5f, (-1.0f + f32(j)) * 2.5f, 0.0f),
+                    .m_rotation = Quat::from_euler(Vec3(0, 0, 0)),
+                },
+                "root_node");
+            game.m_renderer.add_renderable(Renderable{
+                .m_name = id,
+                .m_tags = {"pbr"},
+                .m_mesh = 0,
+                .m_node = game.m_current_level->m_scene.m_nodes_ids[id],
+            });
+        }
+    }
 
     err = game.m_renderer.init(&game);
     if (err) {
