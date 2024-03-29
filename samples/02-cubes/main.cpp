f879daea3816bb17ceff55fd7c42e0c43bb0750a
imadr <hi@imadr.me>
2024-03-29 20:09:56 +0100

    fix 02-cubes

--- samples/02-cubes/main.cpp
+++ samples/02-cubes/main.cpp
@@ -20,14 +20,6 @@ int main() {
     }
 
     game.m_renderer.add_mesh(make_cube());
-    f32 rotation = 1;
-    game.m_current_level->m_scene.add_node(Node{.m_name = "cube", .m_rotation = Quat::from_axis_angle(Vec3(1, 1, 0), rotation)}, "root_node");
-    game.m_renderer.add_renderable(Renderable{
-        .m_name = "cube",
-        .m_tags = {"default"},
-        .m_mesh = 0,
-        .m_node = game.m_current_level->m_scene.m_nodes_ids["cube"],
-    });
 
     err = game.m_renderer.init(&game);
     if (err) {
@@ -35,7 +27,7 @@ int main() {
         return 1;
     }
 
-
+    f32 rotation = 1;
     bool done_screenshot = false;
     while (game.m_window.event_loop()) {
         rotation += 0.01;
