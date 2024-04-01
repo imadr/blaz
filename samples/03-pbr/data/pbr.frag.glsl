bd32fabd2c22768e91d3cf6acb225b2a7550fa47
imad <hi@imadrahmoune.com>
2024-04-02 01:24:40 +0200

    clean pbr shader

--- samples/03-pbr/data/pbr.frag.glsl
+++ samples/03-pbr/data/pbr.frag.glsl
@@ -13,41 +13,36 @@ layout(std140, binding = 1) uniform u_view {
 
 const float PI = 3.14159265359;
 
-////////////////////////
-vec3 fresnelSchlick(float cosTheta, vec3 F0) {
-    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+vec3 fresnel_schlick(float view_dot_halfway, vec3 F0) {
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - view_dot_halfway, 0.0, 1.0), 5.0);
 }
-float DistributionGGX(vec3 N, vec3 H, float roughness) {
+
+float normal_distribution_ggx(vec3 normal, vec3 halfway_vector, float roughness) {
     float a = roughness * roughness;
-    float a2 = a * a;
-    float NdotH = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH * NdotH;
+    float a_2 = a * a;
+    float normal_dot_halfway = max(dot(normal, halfway_vector), 0.0);
+    float normal_dot_halfway_2 = normal_dot_halfway * normal_dot_halfway;
 
-    float num = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
+    float num = a_2;
+    float denom = (normal_dot_halfway_2 * (a_2 - 1.0) + 1.0);
 
-    return num / denom;
+    return a_2 / (PI * denom * denom);
 }
 
-float GeometrySchlickGGX(float NdotV, float roughness) {
+float geometry_schlick_ggx(float normal_dot_view, float roughness) {
     float r = (roughness + 1.0);
     float k = (r * r) / 8.0;
-
-    float num = NdotV;
-    float denom = NdotV * (1.0 - k) + k;
-
-    return num / denom;
+    return normal_dot_view / (normal_dot_view * (1.0 - k) + k);
 }
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness) {
-    float NdotV = max(dot(N, V), 0.0);
-    float NdotL = max(dot(N, L), 0.0);
-    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
-    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+
+float geometry_smith(vec3 normal, vec3 view, vec3 light, float roughness) {
+    float normal_dot_view = max(dot(normal, view), 0.0);
+    float normal_dot_light = max(dot(normal, light), 0.0);
+    float ggx2 = geometry_schlick_ggx(normal_dot_view, roughness);
+    float ggx1 = geometry_schlick_ggx(normal_dot_light, roughness);
 
     return ggx1 * ggx2;
 }
-////////////////////////
 
 void main() {
     vec3 light_position = vec3(1, 2, 2);
@@ -71,27 +66,25 @@ void main() {
 
     vec3 radiance = light_color * attenuation;
 
-    ////////////////////////
-    float NDF = DistributionGGX(v_world_normal, halfway_vector, roughness);
-    float G = GeometrySmith(v_world_normal, view_vector, light_direction, roughness);
-    vec3 F = fresnelSchlick(max(dot(halfway_vector, view_vector), 0.0), F0);
-    vec3 kS = F;
-    vec3 kD = vec3(1.0) - kS;
-    kD *= 1.0 - metallic;
-
-    vec3 numerator = NDF * G * F;
+    float normal_distribution = normal_distribution_ggx(v_world_normal, halfway_vector, roughness);
+    float geometry = geometry_smith(v_world_normal, view_vector, light_direction, roughness);
+    vec3 fresnel = fresnel_schlick(max(dot(halfway_vector, view_vector), 0.0), F0);
+    
+    vec3 numerator = normal_distribution * geometry * fresnel;
     float denominator = 4.0 * max(dot(v_world_normal, view_vector), 0.0) *
                             max(dot(v_world_normal, light_direction), 0.0) +
                         0.0001;
     vec3 specular = numerator / denominator;
-    ////////////////////////
+    
+    vec3 specular_contribution = fresnel;
+    vec3 diffuse_contribution = vec3(1.0) - specular_contribution;
+    diffuse_contribution *= 1.0 - metallic;
+
+    vec3 outgoing_radiance = (diffuse_contribution * albedo / PI + specular) * radiance * diffuse;
 
     vec3 ambient = vec3(0.03) * albedo * ambient_occlusion;
-    vec3 Lo = (kD * albedo / PI + specular) * radiance * diffuse;
 
-    vec3 color = ambient + Lo;
-
-    color = color / (color + vec3(1.0));
+    vec3 color = ambient + outgoing_radiance / (outgoing_radiance + vec3(1.0));
     color = pow(color, vec3(1.0 / 2.2));
 
     o_color = vec4(vec3(color), 1);
