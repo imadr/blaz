61fdaa4c2fa83a67101fe6e3bd8cf1ec53178536
imad <hi@imadrahmoune.com>
2024-04-11 20:11:15 +0200

    moooore wasm stuff

--- src/platform_wasm.cpp
+++ src/platform_wasm.cpp
@@ -10,6 +10,10 @@ Error Window::init(const str& title) {
         EMSCRIPTEN_EVENT_TARGET_WINDOW, this, 0,
         [](int even_type, const EmscriptenUiEvent* ui_event, void* user_data) -> EM_BOOL {
             Window* window = (Window*)user_data;
+            int width = ui_event->windowInnerWidth;
+            int height = ui_event->windowInnerHeight;
+            window->m_size.width = width;
+            window->m_size.height = height;
             window->m_resize_callback(window);
             return EM_TRUE;
         });
