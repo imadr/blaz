a89c71ec7292f36b7e807b93794b0a010dcb3e7a
imadr <hi@imadr.me>
2024-05-16 01:00:56 +0200

    better structs raymarching

--- samples/04-raymarching/data/raymarching.frag.glsl
+++ samples/04-raymarching/data/raymarching.frag.glsl
@@ -10,9 +10,6 @@ layout(std140, binding = 0) uniform u_info {
 
 #define PI 3.14159265359
 
-#define MATERIAL_0 1.0
-#define MATERIAL_1 2.0
-
 mat3 lookat_matrix(vec3 origin, vec3 target, float roll) {
     vec3 rr = vec3(sin(roll), cos(roll), 0.0);
     vec3 ww = normalize(target - origin);
@@ -21,42 +18,120 @@ mat3 lookat_matrix(vec3 origin, vec3 target, float roll) {
     return mat3(uu, vv, ww);
 }
 
-vec2 sphere(vec3 point, float radius, float col) {
-    return vec2(length(point) - radius, col);
+struct Transform {
+    vec3 pos;
+    vec3 rot;
+};
+
+mat3 rotateX(float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+    return mat3(1.0, 0.0, 0.0, 0.0, c, -s, 0.0, s, c);
 }
 
-vec2 plane(vec3 point, vec3 normal, float distance, float col) {
-    return vec2(dot(point, normal) + distance, col);
+mat3 rotateY(float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+    return mat3(c, 0.0, s, 0.0, 1.0, 0.0, -s, 0.0, c);
+}
+
+mat3 rotateZ(float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+    return mat3(c, -s, 0.0, s, c, 0.0, 0.0, 0.0, 1.0);
+}
+
+vec3 rotate(vec3 p, vec3 angles) {
+    mat3 m = rotateX(angles.x) * rotateY(angles.y) * rotateZ(angles.z);
+    return p * m;
+}
+
+vec3 transform(vec3 p, Transform t) {
+    vec3 p_ = p - t.pos;
+    p_ = rotate(p_, t.rot);
+    return p_;
+}
+
+vec2 sphere(vec3 point, float radius, float id) {
+    return vec2(length(point) - radius, id);
+}
+
+vec2 plane(vec3 point, vec3 normal, float distance, float id) {
+    return vec2(dot(point, normal) + distance, id);
 }
 
 vec2 vec_min(vec2 a, vec2 b) {
     return a.x > b.x ? b : a;
 }
 
+#define SPHERE_PRIMITIVE 0
+#define PLANE_PRIMITIVE 1
+
+struct PrimitiveInfo {
+    float sphere_radius;
+    float plane_distance;
+    vec3 plane_normal;
+};
+
+struct Object {
+    int id;
+    int primitive;
+    Transform transform;
+    vec3 color;
+    PrimitiveInfo primitive_info;
+};
+
+vec2 primitive(vec3 point, Object object) {
+    if (object.primitive == SPHERE_PRIMITIVE) {
+        return sphere(transform(point, object.transform), object.primitive_info.sphere_radius,
+                      object.id);
+    } else if (object.primitive == PLANE_PRIMITIVE) {
+        return plane(point, object.primitive_info.plane_normal,
+                     object.primitive_info.plane_distance, object.id);
+    }
+}
+
+#define NUM_OBJECTS 3
+#define PLANE_1 1
+#define SPHERE_1 2
+#define SPHERE_2 3
+const Object objects[NUM_OBJECTS] = {
+    Object(PLANE_1, PLANE_PRIMITIVE, Transform(vec3(0, 0, 0), vec3(0, 0, 0)), vec3(0.6),
+           PrimitiveInfo(0.0, 2.0, vec3(0.0, 1.0, 0.0))),
+    Object(SPHERE_1, SPHERE_PRIMITIVE, Transform(vec3(0, 0, 8), vec3(0, 0, 0)), vec3(1.0, 0.0, 0.0),
+           PrimitiveInfo(1.0, 0.0, vec3(0.0))),
+    Object(SPHERE_2, SPHERE_PRIMITIVE, Transform(vec3(-2, 0, 8), vec3(0, 0, 0)),
+           vec3(1.0, 0.0, 1.0), PrimitiveInfo(1.0, 0.0, vec3(0.0)))};
+
 vec2 scene(vec3 point) {
-    return vec_min(sphere(point - vec3(0, 0, 8), 1., MATERIAL_1),
-                   plane(point, vec3(0.0, 1.0, 0.0), 2.0, MATERIAL_0));
+    vec2 min_ = primitive(point, objects[0]);
+
+    for (int i = 1; i < NUM_OBJECTS; i++) {
+        min_ = vec_min(min_, primitive(point, objects[i]));
+    }
+
+    return min_;
 }
 
 vec2 march(vec3 ray_origin, vec3 ray_direction) {
     vec3 current_point = ray_origin;
     float dist_total = 0.;
-    float col;
+    float id;
     for (int i = 0; i < 3000; i++) {
         current_point = ray_origin + ray_direction * dist_total;
         vec2 s = scene(current_point);
-        col = s.y;
+        id = s.y;
         float dist = s.x;
         dist_total += dist;
         if (dist < 0.00001) {
             break;
         }
         if (dist_total > 1000.) {
-            col = 0.;
+            id = 0.;
             break;
         }
     }
-    return vec2(dist_total, col);
+    return vec2(dist_total, id);
 }
 
 vec3 normal(vec3 point) {
@@ -78,14 +153,6 @@ float lighting(vec3 point, vec3 normal, vec3 light_pos) {
     return max(0., dot(normal, direction_to_light));
 }
 
-vec3 material(float index) {
-    if (index == 1.0) {
-        return vec3(0.6);
-    } else if (index == 2.0) {
-        return vec3(1.0, 0.0, 0.0);
-    }
-}
-
 void main() {
     vec2 uv = gl_FragCoord.xy / u_resolution.xy - 0.5;
     uv.x *= u_resolution.x / u_resolution.y;
@@ -98,7 +165,7 @@ void main() {
 
     vec2 march_out = march(camera_pos, view);
     float depth = march_out.x;
-    vec3 col = material(march_out.y);
+    vec3 col = objects[int(march_out.y) - 1].color;
 
     if (march_out.y == 0.0) {
         o_color = mix(vec4(0.80, 0.94, 0.99, 1.), vec4(0., 0.72, 0.96, 1.), uv.y * 3.);
