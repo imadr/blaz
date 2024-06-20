fd9e7738d7e2a5b967fe907dbb0e009d46e4f07f
imad <hi@imadrahmoune.com>
2024-06-20 15:32:27 +0200

    fix cmake work dir/data dir config debug release

--- samples/Common.cmake
+++ samples/Common.cmake
@@ -16,14 +16,16 @@ if (EMSCRIPTEN)
         "${CMAKE_CURRENT_BINARY_DIR}/index.html"
     )
 else()
+    set_property(TARGET ${SAMPLE_NAME} PROPERTY VS_DEBUGGER_WORKING_DIRECTORY "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>")
+
     add_custom_target(${SAMPLE_NAME}_DATA
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/data ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/data
+        COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/data ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>/data
     )
     set_target_properties(${SAMPLE_NAME}_DATA PROPERTIES FOLDER ${MISC_TARGET_FOLDER})
     add_dependencies(${SAMPLE_NAME} ${SAMPLE_NAME}_DATA)
 
     add_custom_target(${SAMPLE_NAME}_INTERNAL_DATA
-        COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_SOURCE_DIR}/../internal_shaders/" ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/internal_data
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_CURRENT_SOURCE_DIR}/../internal_shaders/" ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>/internal_data
     )
     set_target_properties(${SAMPLE_NAME}_INTERNAL_DATA PROPERTIES FOLDER ${MISC_TARGET_FOLDER})
     add_dependencies(${SAMPLE_NAME} ${SAMPLE_NAME}_INTERNAL_DATA)
