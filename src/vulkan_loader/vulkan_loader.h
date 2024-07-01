5a510a8128d4273d2873bc25354e9283d077e6b6
imadr <hi@imadr.me>
2024-07-01 17:07:01 +0200

    vulkan function loading macro

--- src/vulkan_loader/vulkan_loader.h
+++ src/vulkan_loader/vulkan_loader.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #include "error.h"
+#include "types.h"
 #include "vulkan_loader/vulkan_header.h"
 
 namespace blaz {
 
+#define VULKAN_FUNCTIONS_LIST         \
+    VULKAN_FUNCTION(vkCreateInstance) \
+    VULKAN_FUNCTION(vkEnumerateInstanceExtensionProperties)
+
 struct VulkanLoader {
     Error init();
+    void* load_function(str name);
+    PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr;
 };
 
 }  // namespace blaz
