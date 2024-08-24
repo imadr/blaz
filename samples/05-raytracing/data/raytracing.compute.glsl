abb834c569cf98506176c598e80d82ba9e89d8c4
imadr <hi@imadr.me>
2024-08-24 15:31:33 +0200

    fix raytracing random sampling

--- samples/05-raytracing/data/raytracing.compute.glsl
+++ samples/05-raytracing/data/raytracing.compute.glsl
@@ -141,24 +141,22 @@ void ray_triangle_intersection(Ray ray, float ray_min, float ray_max, Hittable t
 #define RAY_MIN 0.0001
 #define RAY_MAX 999999
 #define SAMPLE_PER_RAY 1
-#define MAX_BOUNCES 30
+#define MAX_BOUNCES 40
 
-float hash(float n) {
-    return fract(sin(n) * 43758.5453);
+uint next_random(inout uint rng_state) {
+    rng_state = rng_state * 747796405 + 2891336453;
+    uint result = ((rng_state >> ((rng_state >> 28) + 4)) ^ rng_state) * 277803737;
+    result = (result >> 22) ^ result;
+    return result;
 }
 
-vec2 hash(vec2 p) {
-    return vec2(hash(p.x + p.y * 57.0), hash(p.y + p.x * 57.0));
+float random(inout uint rng_state) {
+    return next_random(rng_state) / 4294967295.0;
 }
 
-// [0, 1]
-float random(vec2 uv) {
-    return fract(sin(dot(uv.xy, vec2(12.9898, 78.233))) * 43758.5453123 * hash(u_frame_number));
-}
-
-vec3 random_unit_vector(vec2 uv) {
-    float rand1 = random(uv);
-    float rand2 = random(uv + 1.0);
+vec3 random_unit_vector(inout uint rng_state) {
+    float rand1 = random(rng_state);
+    float rand2 = random(rng_state);
 
     float theta = rand1 * 2.0 * 3.14159265358979323846;
     float phi = acos(2.0 * rand2 - 1.0);
@@ -170,8 +168,8 @@ vec3 random_unit_vector(vec2 uv) {
     return normalize(vec3(x, y, z));
 }
 
-vec3 random_unit_vector_on_hemisphere(vec2 uv, vec3 normal) {
-    vec3 v = random_unit_vector(uv);
+vec3 random_unit_vector_on_hemisphere(inout uint rng_state, vec3 normal) {
+    vec3 v = random_unit_vector(rng_state);
     if (dot(v, normal) > 0.0) {
         return v;
     } else {
@@ -187,18 +185,20 @@ bool near_zero(vec3 v) {
 #define PRIMITIVE_SPHERE 0
 #define PRIMITIVE_TRIANGLE 1
 
-const uint num_materials = 4;
+const uint num_materials = 5;
 const Material materials[num_materials] = Material[](
-    Material(vec3(0), NON_METAL, 0, vec3(10)), Material(vec3(0, 1, 0), NON_METAL, 0, vec3(0)),
-    Material(vec3(1, 0, 0), NON_METAL, 0, vec3(0)), Material(vec3(1), NON_METAL, 0, vec3(0)));
+    Material(vec3(0), NON_METAL, 0, vec3(20)), Material(vec3(0, 1, 0), NON_METAL, 0, vec3(0)),
+    Material(vec3(1, 0, 0), NON_METAL, 0, vec3(0)), Material(vec3(1), NON_METAL, 0, vec3(0)),
+    Material(vec3(1), METAL, 0, vec3(0)));
 #define MATERIAL_WHITE_EMISSIVE 0
 #define MATERIAL_GREEN 1
 #define MATERIAL_RED 2
 #define MATERIAL_WHITE 3
+#define MATERIAL_MIRROR 4
 
 const Triangle default_triangle = Triangle(vec3(0), vec3(0), vec3(0));
 
-const uint num_hittables = 13;
+const uint num_hittables = 14;
 const Hittable hittables[num_hittables] = Hittable[](
     Hittable(vec3(0, 0, 0), MATERIAL_WHITE, PRIMITIVE_TRIANGLE, 0, vec3(0, 1, 0),
              Triangle(vec3(-1, -0.5, -0.5), vec3(1, -0.5, -0.5), vec3(1, -0.5, 1.5))),
@@ -226,27 +226,30 @@ const Hittable hittables[num_hittables] = Hittable[](
              Triangle(vec3(1, 1.5, 1.5), vec3(1, 1.5, -0.5), vec3(-1, 1.5, 1.5))),
 
     Hittable(vec3(0, 0.5, 0), MATERIAL_WHITE_EMISSIVE, PRIMITIVE_TRIANGLE, 0, vec3(0, -1, 0),
-             Triangle(vec3(-0.5, 0.99, -0.5), vec3(0.5, 0.99, -0.5), vec3(-0.5, 0.99, 1))),
+             Triangle(vec3(-0.5, 0.99, 0), vec3(0.5, 0.99, 0), vec3(-0.5, 0.99, 1))),
     Hittable(vec3(0, 0.5, 0), MATERIAL_WHITE_EMISSIVE, PRIMITIVE_TRIANGLE, 0, vec3(0, -1, 0),
-             Triangle(vec3(0.5, 0.99, 1), vec3(0.5, 0.99, -0.5), vec3(-0.5, 0.99, 1))),
+             Triangle(vec3(0.5, 0.99, 1), vec3(0.5, 0.99, 0), vec3(-0.5, 0.99, 1))),
 
-    Hittable(vec3(0, 0, 0), MATERIAL_WHITE, PRIMITIVE_SPHERE, 0.5, vec3(0), default_triangle)
+    Hittable(vec3(0.4, -0.2, 0), MATERIAL_MIRROR, PRIMITIVE_SPHERE, 0.4, vec3(0), default_triangle),
+    Hittable(vec3(-0.4, -0.2, 0), MATERIAL_WHITE, PRIMITIVE_SPHERE, 0.4, vec3(0), default_triangle)
 
 );
 
 void main() {
     ivec2 texel_coord = ivec2(gl_GlobalInvocationID.xy);
-    vec2 resolution = imageSize(u_image_render);
-    vec2 uv = texel_coord / resolution.xy - 0.5;
-    uv.x *= resolution.x / resolution.y;
+    ivec2 resolution = imageSize(u_image_render);
+    vec2 uv = texel_coord / vec2(resolution.xy) - 0.5;
+    uv.x *= float(resolution.x) / float(resolution.y);
     float fov = 2.0;
     uv *= fov;
 
     mat3 matrix = lookat_matrix(u_camera_position, u_camera_target, 0.0);
 
+    uint rng_state = texel_coord.y * resolution.x + texel_coord.x + u_frame_number * 1281565;
+
     vec3 final_color = vec3(0.0);
     for (int i = 0; i < SAMPLE_PER_RAY; i++) {
-        vec2 offset = vec2(random(uv + vec2(i)), random(uv + random(uv) + vec2(i))) - vec2(0.5);
+        vec2 offset = vec2(random(rng_state), random(rng_state)) - vec2(0.5);
         offset /= resolution.xy;
 
         vec3 view = matrix * normalize(vec3(uv + offset, 1.0));
@@ -277,15 +280,16 @@ void main() {
 
                 vec3 material_color;
                 if (hit_material.metal) {
-                    vec3 reflected_direction = normalize(reflect(ray.direction, hit.normal)) +
-                                               hit_material.roughness * random_unit_vector(uv);
+                    vec3 reflected_direction =
+                        normalize(reflect(ray.direction, hit.normal)) +
+                        hit_material.roughness * random_unit_vector(rng_state);
                     ray = Ray(hit.point, reflected_direction);
 
                     if (dot(ray.direction, hit.normal) > 0) {
                         material_color = hit_material.albedo;
                     }
                 } else {
-                    vec3 scatter_direction = hit.normal + random_unit_vector(uv);
+                    vec3 scatter_direction = hit.normal + random_unit_vector(rng_state);
 
                     if (near_zero(scatter_direction)) {
                         scatter_direction = hit.normal;
