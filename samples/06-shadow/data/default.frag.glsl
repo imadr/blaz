36a56adfba2b1df28d31214bdf7f963ba1e426e6
imad <hi@imadrahmoune.com>
2024-09-24 21:58:20 +0200

    pcf shadow and bunny mesh

--- samples/06-shadow/data/default.frag.glsl
+++ samples/06-shadow/data/default.frag.glsl
@@ -16,16 +16,25 @@ layout(std140, binding = 1) uniform u_light {
 
 layout(binding = 2) uniform sampler2D u_sampler_shadowmap;
 
-#define SHADOW_BIAS 0.00001
+#define SHADOW_BIAS 0.00004
+#define NUM_SAMPLES 3
 
 void main() {
     vec3 projected_light_coords = v_light_space_position.xyz / v_light_space_position.w;
     projected_light_coords = projected_light_coords * 0.5 + 0.5;
-    float shadowmap_sample = texture(u_sampler_shadowmap, projected_light_coords.xy).r;
     vec3 light_direction = normalize(u_light_position - v_world_position);
-    vec3 color = vec3(dot(v_normal, light_direction));
-    if (shadowmap_sample < projected_light_coords.z - SHADOW_BIAS) {
-        color = vec3(0);
+
+    vec2 texel_size = 1.0 / textureSize(u_sampler_shadowmap, 0);
+
+    float shadow = 0.0;
+    for (int x = -NUM_SAMPLES; x <= NUM_SAMPLES; x++) {
+        for (int y = -NUM_SAMPLES; y <= NUM_SAMPLES; y++) {
+            float shadowmap_sample =
+                texture(u_sampler_shadowmap, projected_light_coords.xy + vec2(x, y) * texel_size).r;
+            shadow += shadowmap_sample > projected_light_coords.z - SHADOW_BIAS ? 1.0 : 0.0;
+        }
     }
-    o_color = vec4(color, 1.0);
+    shadow /= (2 * NUM_SAMPLES + 1) * (2 * NUM_SAMPLES + 1);
+    vec3 color = vec3(dot(v_normal, light_direction));
+    o_color = vec4(color * shadow, 1.0);
 }
