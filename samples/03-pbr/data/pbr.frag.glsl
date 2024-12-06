cbcd5cf97058895872a036bcecbd68083c808796
imadr <hi@imadr.me>
2024-12-06 17:40:17 +0100

    .

--- samples/03-pbr/data/pbr.frag.glsl
+++ samples/03-pbr/data/pbr.frag.glsl
@@ -1,10 +1,9 @@
 #version 450
 
-layout(location = 0) in vec3 v_position;
-layout(location = 1) in vec3 v_world_position;
-layout(location = 2) in vec3 v_world_normal;
-layout(location = 3) in vec3 v_world_tangent;
-layout(location = 4) in vec2 v_texcoord;
+layout(location = 0) in vec3 v_world_position;
+layout(location = 1) in vec3 v_world_normal;
+layout(location = 2) in vec3 v_world_tangent;
+layout(location = 3) in vec2 v_texcoord;
 
 layout(location = 0) out vec4 o_color;
 
